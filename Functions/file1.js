d2507585935901f9835ce1e905ac7e093947bd7b
Shadrack kipkorir <shadybett540@gmail.com>
2023-09-27 06:29:33 +0300

    normal practice

---
+++ Functions/file1.js
@@ -0,0 +1,9 @@
+let series = ['marked heart','the oath','sex education','elite']
+let num = [1,2,3,4,5]
+let people = ['kim','kip','kip']
+function seriesArray(array){
+    console.log (array)
+}
+seriesArray (series)
+seriesArray (num)
+seriesArray (people)
